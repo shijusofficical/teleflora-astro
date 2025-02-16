043e04e8df2066641247e876966b1eff9edca27b
ShijuS <shijusofficial@gmail.com>
2025-02-16 23:18:20 +0530

    added sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,14 @@ import react from '@astrojs/react';
 import compressor from 'astro-compressor';
 import partytown from '@astrojs/partytown';
 
+import sentry from '@sentry/astro';
+
 export default defineConfig({
-  integrations: [react(), compressor(), partytown()],
+  integrations: [react(), compressor(), partytown(), sentry({
+    dsn: "https://c34f54ee5c292be9496bdf39b768b764@o4508098119335936.ingest.de.sentry.io/4508830116479056",
+    sourceMapsUploadOptions: {
+      project: "teleflora-poc",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    },
+  }),],
 });
